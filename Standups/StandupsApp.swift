5b05bcdee2766c604461ec327be7caffdc104c5b
Dave Reed <dave256@mac.com>
2023-02-10 18:35:41 -0500

    Initial Commit

---
+++ Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by David Reed on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
